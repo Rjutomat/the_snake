54cbd86f987856bfeb43104f086e9c9f3f529d9c
Rjutomat <81323616+Rjutomat@users.noreply.github.com>
2024-08-07 18:33:50 +0300

    Add files via upload

--- the_snake.py
+++ the_snake.py
@@ -110,18 +110,18 @@ class Snake(GameObject):
         """Метод движения и изменения движения объекта класса."""
         head_position = self.get_head_position()
         new_head_position = ((
-                            head_position[0]
-                            + self.direction[0]
-                            * GRID_SIZE
-                            )
+                               head_position[0]
+                               + self.direction[0]
+                               * GRID_SIZE
+                              )
                              % SCREEN_WIDTH,
                              (
-                            head_position[1]
-                            + self.direction[1]
-                            * GRID_SIZE
-                            )
+                               head_position[1]
+                               + self.direction[1]
+                               * GRID_SIZE
+                             )
                              % SCREEN_HEIGHT
-                            )
+                             )
         self.positions.insert(0, new_head_position)
         if len(self.positions) > self.length:
             self.last_segment = self.positions.pop()
@@ -144,7 +144,7 @@ class Snake(GameObject):
                 self.last_segment,
                 BOARD_BACKGROUND_COLOR,
                 border_color=BOARD_BACKGROUND_COLOR
-            )
+                )
 
     def get_head_position(self):
         """Метод определения координат первого элемента."""
@@ -168,7 +168,7 @@ def handle_keys(game_object):
             game_object.update_direction(DIRECTIONS.get((
                                                          game_object.direction,
                                                          event.key
-                                                        ),
+                                                         ),
                                                         game_object.direction
                                                         ))
 
